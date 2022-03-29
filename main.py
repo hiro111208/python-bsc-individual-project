4841a2e67b4b6aa1b706c2603c91826c8927681e
Hiro Funatsuka <hiro.funatsuka@kcl.ac.uk>
2022-03-29 06:48:25 +0100

    Add a function of exporting excel file

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from failure_probability import FailureProbability
 from cost import Cost
 import random
 
+import openpyxl
+
 from typing import Dict
 
 from copy import deepcopy
@@ -36,6 +38,9 @@ def price_of_anarchy_v0(num_players, num_resources, benefit, start_cost, start_p
 
 def price_of_anarchy_v1(num_players, num_resources, benefit, start_cost, start_probability):
     players = dict()
+    initial_benefit = benefit
+    initial_cost = start_cost
+    initial_fail = 1 - 1 / (1 + 1 / 10 * start_probability)
     for i in range(1, num_players + 1):
         players[i] = Player(i, benefit)
         benefit /= 5
@@ -43,8 +48,8 @@ def price_of_anarchy_v1(num_players, num_resources, benefit, start_cost, start_p
     failure_probability = dict()
     rand_nums = sorted([random.randint(1, 100) for i in range(len(players))])
     for i in range(1, num_players + 1):
-        failure_probability[i] = i / 100
-        #failure_probability[i] = 1 - 1 / (1 + i * start_probability)
+        #failure_probability[i] = i*9 / 100
+        failure_probability[i] = 1 - 1 / (1 + i / 10 * start_probability)
     for i in range(1, num_players + 1):
         cost[i] = i * start_cost
     resources: Dict[int, Resource] = dict()
@@ -56,11 +61,18 @@ def price_of_anarchy_v1(num_players, num_resources, benefit, start_cost, start_p
     equilibrium_profile: StrategyProfile = Equilibrium(players, resources)
     social_optima: float = optimal_profile.social_utility
     if social_optima != 0:
-        print(f'Price of Anarchy: {social_optima / equilibrium_profile.profile.social_utility}')
+        price_of_anarchy = social_optima / equilibrium_profile.profile.social_utility
+        #print(f'Price of Anarchy: {social_optima / equilibrium_profile.profile.social_utility}')
+        return (initial_fail, price_of_anarchy, social_optima, equilibrium_profile.profile.social_utility)
     else:
-        print(f'Equilibrium utility: {equilibrium_profile.profile.social_utility}')
-""" for cost in range(1, 100):
-    price_of_anarchy_v1(2,2,10,cost,1) """
+        #print(f'Equilibrium utility: {equilibrium_profile.profile.social_utility}')
+        return (initial_fail, None, social_optima, equilibrium_profile.profile.social_utility)
+""" for player in range(2, 11):
+    print(price_of_anarchy_v1(player,2,100,1,1)) """
+
+dataset = [price_of_anarchy_v1(3,3,100,1,fp) for fp in range(0,101)]
+
+
 
 def price_of_anarchy_v2(max_players, max_resources, max_benefit, max_cost, max_probability):
     for num_players in range(2, max_players + 1):
@@ -77,8 +89,8 @@ def price_of_anarchy_v2(max_players, max_resources, max_benefit, max_cost, max_p
                         rand_nums = sorted([random.randint(1, 100) for i in range(len(players))])
                         for i in range(1, num_players + 1):
                             #failure_probability[i] = rand_nums[i-1] / 100
-                            #failure_probability[i] = i / 100
-                            failure_probability[i] = 1 - 1 / (1 + i * start_probability)
+                            failure_probability[i] = i / 100
+                            #failure_probability[i] = 1 - 1 / (1 + i * start_probability)
                         for i in range(1, num_players + 1):
                             cost[i] = i * start_cost
                         resources: Dict[int, Resource] = dict()
@@ -110,7 +122,8 @@ def check_algorithm(num_players, num_resources, benefit):
     rand_nums = sorted([random.randint(1, 100) for i in range(len(players))])
     for i in range(1, num_players + 1):
         #failure_probability[i] = rand_nums[i-1] / 100
-        failure_probability[i] = 1 - 1 / (1 + i)
+        #failure_probability[i] = 1 - 1 / (1 + i)
+        failure_probability[i] = i / 100
     for i in range(1, num_players + 1):
         cost[i] = i
     resources: Dict[int, Resource] = dict()
@@ -122,6 +135,23 @@ def check_algorithm(num_players, num_resources, benefit):
     equilibrium_profile.display_result()
 
 #check_algorithm(2, 10, 10)
+
+def export_excel(data):
+    wb = openpyxl.Workbook()
+
+    # Check Sheet
+    print(f'Sheet name： {wb.get_sheet_names()}')
+
+    # Retrieve sheet object
+    s1 = wb.get_sheet_by_name(wb.get_sheet_names()[0])
+
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            s1.cell(row=i+1,column=j+1,value=data[i][j])
+
+    wb.save('test.xlsx')
+
+# export_excel(dataset)
 
 """ cost = dict()
 failure_probability = dict()
